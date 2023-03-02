b68a6d4289da40758a857e05a0309ee12963ab62
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2023-03-02 02:05:27 -0500

    Update List.h

--- List.h
+++ List.h
@@ -1,1 +1,20 @@
+#pragma once
+#include <iostream>
+#include "Node.h"
 
+class List{
+ private:
+  Node *head;
+ public:
+  List();
+  void insert(std::string data);
+  void insert(int loc, std::string data); //0 indexed
+  int length();
+
+  std::string toString(); // for testing purposes
+    void remove(int loc);
+    ~List();
+    bool contains(std::string item);
+
+
+    };
