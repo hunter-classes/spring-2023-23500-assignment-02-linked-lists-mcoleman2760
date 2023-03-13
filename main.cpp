d80e00cb0fd17999532b91046ef8befbf685b09c
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2023-03-13 02:43:39 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "List.h"
+#include "OList.h"
 
 int main()
 {
@@ -30,5 +31,46 @@ int main()
 	std::cout << l->toString() << " " << l->length() << "\n"; // length is now 5 since one item is removed
     l->remove(4);
     std::cout << l->toString() << " " << l->length() << "\n"; // length is now 4 since another item is removed from the end
-  return 0;
+
+// OList
+std::cout << "---------" << std::endl;
+std::cout << "OList" << std::endl;
+std::cout << "---------" << std::endl;
+
+OList *o = new OList();
+o->insert("hi");
+o->insert("bye");
+o->insert("sad");
+o->insert("mad");
+o->insert("glad");
+o->insert("father");
+
+std::cout << "toString function" << std::endl;
+std::cout << o->toString() << std::endl;
+
+std::cout << "contains function" << std::endl;
+std::cout << o -> contains("father") << "\n" << o-> contains("glad") << "\n" << o -> contains("mad") << "\n" << o -> contains("sad") << "\n" <<  o -> contains("bye") << "\n" << o -> contains("hi") << "\n" << o -> contains("money") << std::endl ;
+
+std::cout << "get function" << std::endl;
+std::cout << "Word at loc 4: " << o-> get(4) << std::endl; 
+
+std::cout << "remove function " << std::endl;
+std::cout << "removing data at loc 4 " << std::endl;
+o->remove(4);
+std::cout << o->toString() << std::endl;
+
+
+std::cout << "reverse function" << std::endl;
+o->reverse();
+std::cout << o->toString() << "\n"; 
+
+std::cout << "length function" << std::endl;
+std::cout << o->length() << "\n";
+
+std::cout << "Destructor" << std::endl;
+delete o;
+std::cout << o->toString() << "\n"; 
+
+
+return 0;
 }
